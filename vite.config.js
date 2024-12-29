fd6c60477ad9ed4d5915c6488617f8accf9a5be4
maks.sem <maks.sem@fernir.co>
2024-12-29 13:41:46 +0200

    add-base-path

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-todo-app_composition-api/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
